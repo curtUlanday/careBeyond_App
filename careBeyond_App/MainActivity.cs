e59d7e4926fc9836080635d39f7222f2813ac792
Curt Ulanday <curtulanday0430@live.com>
2017-10-17 12:46:00 +0800

    0.1 Release - Navigation Menu
    Created navigation menu for main view

--- careBeyond_App/MainActivity.cs
+++ careBeyond_App/MainActivity.cs
@@ -1,18 +1,66 @@
-﻿using Android.App;
-using Android.Widget;
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
 using Android.OS;
+using Android.Support.V7.App;
+using Android.Support.Design.Widget;
+using Android.Support.V7.Widget;
+using Android.Support.V4.Widget;
 
 namespace careBeyond_App
 {
     [Activity(Label = "careBeyond_App", MainLauncher = true)]
-    public class MainActivity : Activity
+    public class MainActivity : AppCompatActivity
     {
+        DrawerLayout drawerLayout;
+        Toolbar toolbar;
+        NavigationView navigationView;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
+            drawerLayout = FindViewById<DrawerLayout>(Resource.Id.drawerLayout);
+            toolbar = FindViewById<Toolbar>(Resource.Id.toolbar);
+            SetSupportActionBar(toolbar);
+            SupportActionBar.SetDisplayHomeAsUpEnabled(true);
+            SupportActionBar.SetHomeButtonEnabled(true);
+
+            SupportActionBar.SetHomeAsUpIndicator(Resource.Drawable.ic_list);
+
+            navigationView = FindViewById<NavigationView>(Resource.Id.navLayout);
+            navigationView.NavigationItemSelected += NavView_NavigationItemSelected;
+
+           
+        }
+
+        private void NavView_NavigationItemSelected(object sender, NavigationView.NavigationItemSelectedEventArgs e)
+        {
+            e.MenuItem.SetChecked(true);
+
+            switch (e.MenuItem.ItemId)
+            {
+                case Resource.Id.action_save:
+                    break;
+
+            }
+            Snackbar.Make(drawerLayout, "Selected: " + e.MenuItem.TitleFormatted, Snackbar.LengthLong).Show();
+            drawerLayout.CloseDrawers();
+
+        }
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            switch (item.ItemId)
+            {
+                case Android.Resource.Id.Home:
+                    drawerLayout.OpenDrawer(Android.Support.V4.View.GravityCompat.Start);
+                    return true;
+            }
+            return base.OnOptionsItemSelected(item);
         }
     }
 }
